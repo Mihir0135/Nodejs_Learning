e46c1c4a452d1dcaae45b3d040997190c3f1061d
Mihir Prajapati <mihir.p@brilworks.com>
2024-09-10 10:13:21 +0530

    make some changes on file module task

--- Read_Write_File/index.js
+++ Read_Write_File/index.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
+const read = 'read.txt';
 
-const beforeReadFile = fs.readFileSync('read.txt','utf-8');
+function readFile() {
+    try {
+        const data = fs.readFileSync('read.txt','utf-8');
+        console.log(data);
+    } catch (error) {
+        console.log('File not found');
+    }
+}
+
 console.log('Before Appending:');
-console.log(beforeReadFile);
+readFile();
 
 ['Paython', 'Laravel', 'Ruby','GraphQL', 'MongoDB', 'NestJS'].forEach((item) => {
     fs.appendFileSync('read.txt',`\n${item}`);
 })
 
 console.log('-----------------------------------');
-const afterReadFile = fs.readFileSync('read.txt','utf-8');
 console.log('After Appending:');
-console.log(afterReadFile);
+readFile();
